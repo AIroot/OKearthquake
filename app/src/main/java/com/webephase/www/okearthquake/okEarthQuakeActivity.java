439663865cbba79f3358cd7fceb391c0f868b71c
AI1988 <aijpathirana@gmail.com>
2017-05-30 10:20:40 -0500

    Create okEarthQuake main activity

---
+++ app/src/main/java/com/webephase/www/okearthquake/okEarthQuakeActivity.java
@@ -0,0 +1,13 @@
+package com.webephase.www.okearthquake;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class okEarthQuakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ok_earth_quake);
+    }
+}
