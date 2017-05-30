17e963086df8f6e597bc12db6195b4b67ba47ece
AI1988 <aijpathirana@gmail.com>
2017-05-30 11:18:00 -0500

    Create list of location OkEarthQuake main activity

--- app/src/main/java/com/webephase/www/okearthquake/OkEarthQuakeActivity.java
+++ app/src/main/java/com/webephase/www/okearthquake/OkEarthQuakeActivity.java
@@ -3,7 +3,7 @@ package com.webephase.www.okearthquake;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class okEarthQuakeActivity extends AppCompatActivity {
+public class OkEarthQuakeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
